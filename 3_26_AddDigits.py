7dba32da1400f9bf52ac9b4626eeb3aed162bc12
Cassie07 <31313145+Cassie07@users.noreply.github.com>
2018-03-26 11:00:49 -0400

    Update 3_26_AddDigits.py

--- 3_26_AddDigits.py
+++ 3_26_AddDigits.py
@@ -1,3 +1,19 @@
+'''258. Add Digits
+
+DescriptionHintsSubmissionsDiscussSolution
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+'''
+
 '''solution 1'''
 '''(runtime beats 90%)i separate the num(38) in to a string('38') and then change it into a list(['3','8'])'''
 class Solution:
