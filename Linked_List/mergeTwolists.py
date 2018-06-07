9132e70b8accaad1c91fbdbc2c27a41f9f426974
Cassie07 <31313145+Cassie07@users.noreply.github.com>
2018-06-07 19:22:21 +0800

    Update mergeTwolists.py

--- Linked_List/mergeTwolists.py
+++ Linked_List/mergeTwolists.py
@@ -1,11 +1,12 @@
-Question Description:
-Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+# Question Description:
+# Merge two sorted linked lists and return it as a new list. 
+# The new list should be made by splicing together the nodes of the first two lists.
 
-Example:
+# Example:
 
-Input: 1->2->4, 1->3->4
-Output: 1->1->2->3->4->4
-
+# Input: 1->2->4, 1->3->4
+# Output: 1->1->2->3->4->4
+# ************************************************************************************
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
