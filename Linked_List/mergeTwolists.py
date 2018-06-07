4d14173e59547f382151a6a6cd219305bdc436cf
Cassie07 <31313145+Cassie07@users.noreply.github.com>
2018-06-07 19:25:33 +0800

    Update mergeTwolists.py

--- Linked_List/mergeTwolists.py
+++ Linked_List/mergeTwolists.py
@@ -1,11 +1,15 @@
+# 21. Merge two lists
 # Question Description:
 # Merge two sorted linked lists and return it as a new list. 
 # The new list should be made by splicing together the nodes of the first two lists.
 
 # Example:
-
 # Input: 1->2->4, 1->3->4
 # Output: 1->1->2->3->4->4
+
+# Solution:
+# Compare the elements in two lists. Add the smaller one into the list and update the pointer. 
+# When one of the lists is traversed, add the rest elements of another list into the returned list directly.
 # ************************************************************************************
 # Definition for singly-linked list.
 # class ListNode:
@@ -20,7 +24,7 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        head=new=ListNode(0)
+        head=new=ListNode(0)   # *
         while l1 and l2:
             if l1.val<=l2.val:
                 head.next=l1
@@ -28,9 +32,10 @@ class Solution:
             else:
                 head.next=l2
                 l2=l2.next
-            head=head.next
+            head=head.next   # *
         if l1:
             head.next=l1
         else:
             head.next=l2
         return new.next
+    
