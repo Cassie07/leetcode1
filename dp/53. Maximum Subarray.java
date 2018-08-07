866d85c99c7fccec7abf2129dee86855360d4549
Cassie07 <31313145+Cassie07@users.noreply.github.com>
2018-08-07 17:34:28 +0800

    Update 53. Maximum Subarray.java

--- dp/53. Maximum Subarray.java
+++ dp/53. Maximum Subarray.java
@@ -12,8 +12,27 @@ Follow up:
 
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 */
+// Second version =======> running time: O(n) ========> DP version
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=0;
+        int res=nums[0];
+        int n=nums.length;
+        int[] dp=new int[n];
+        for (int i=0;i<n;i++){
+            dp[i]=Math.max(i==0? nums[i]:dp[i-1]+nums[i],nums[i]); // transfer function: dp[i]=max(dp[i-1]+nums[i],nums[i])
+            res=Math.max(dp[i],res);
+        }
+        return res;
+        
+        /** dp[i] is the max sum of i elements. Thus dp[i-1] had been calculated. It means the biggest sum of i-1 elements has
+            been calculated. if dp[i-1]+nums[i] not larger than nums[i], it means that the biggest sum of subarray in i elements 
+            should be nums[i] itself.
+        */
+    }
+}
 
-// First version ========> running time: O(n^2)
+// First version  =======> running time: O(n^2)
 class Solution {
     public int maxSubArray(int[] nums) {
         int max=0;
